9814648cbbf124b0c83897d5cb4b9195b5178d99
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2023-11-13 14:48:05 +0100

    Add simple Nameplate example.

--- bundles/io.github.linkedfactory.service/src/test/scala/io/github/linkedfactory/service/rdf4j/aas/ServiceTest.java
+++ bundles/io.github.linkedfactory.service/src/test/scala/io/github/linkedfactory/service/rdf4j/aas/ServiceTest.java
@@ -61,6 +61,28 @@ public class ServiceTest {
 	}
 
 	@Test
+	public void findNameplateTest() {
+		try (RepositoryConnection conn = repository.getConnection()) {
+			String query = "prefix aas: <https://admin-shell.io/aas/3/0/> " +
+					"select ?sm ?element ?p ?o { " +
+					"service <aas-api:https://v3.admin-shell-io.com> { " +
+					"{ select ?shell { <aas-api:endpoint> <aas-api:shells> ?shell } limit 2 } " +
+					"?shell aas:submodels ?sm . ?sm aas:semanticId ?semId . ?semId aas:keys [ !<:> ?key ] . "+
+					"?key aas:value \"https://admin-shell.io/zvei/nameplate/1/0/Nameplate\" . " +
+					"?sm (!<:>)+ ?element . " +
+					"{ ?element a aas:Property } union { ?element a aas:MultiLanguageProperty } " +
+					"?element ?p ?o . " +
+					"} " +
+					"}";
+			try (TupleQueryResult result = conn.prepareTupleQuery(query).evaluate()) {
+				while (result.hasNext()) {
+					System.out.println(result.next());
+				}
+			}
+		}
+	}
+
+	@Test
 	public void submodelsTest() {
 		try (RepositoryConnection conn = repository.getConnection()) {
 			String query = "prefix aas: <https://admin-shell.io/aas/3/0/> " +
