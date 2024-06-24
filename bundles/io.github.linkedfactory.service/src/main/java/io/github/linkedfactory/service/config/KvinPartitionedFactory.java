c0114fc9f611e413c60c43156a7fc639d4c62bd1
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2024-06-24 22:42:06 +0200

    Allow usage of duration expressions.

--- bundles/io.github.linkedfactory.service/src/main/java/io/github/linkedfactory/service/config/KvinPartitionedFactory.java
+++ bundles/io.github.linkedfactory.service/src/main/java/io/github/linkedfactory/service/config/KvinPartitionedFactory.java
@@ -3,11 +3,14 @@ package io.github.linkedfactory.service.config;
 import io.github.linkedfactory.core.kvin.Kvin;
 import io.github.linkedfactory.core.kvin.partitioned.KvinPartitioned;
 import net.enilink.composition.annotations.Iri;
+import net.enilink.komma.core.ILiteral;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.format.DateTimeParseException;
 
 @Iri("plugin://io.github.linkedfactory.service/data/KvinPartitioned")
 public abstract class KvinPartitionedFactory extends KvinLevelDbFactory {
@@ -17,15 +20,31 @@ public abstract class KvinPartitionedFactory extends KvinLevelDbFactory {
 	public Kvin create() {
 		try {
 			File archivePath = getStorePAthOr("linkedfactory-partition");
-			int archiveInterval = getArchiveInterval();
+			ILiteral archiveInterval = getArchiveInterval();
 			log.info("Using path: {} for archiving", archivePath);
 
-			return new KvinPartitioned(archivePath, archiveInterval);
+			Duration archiveIntervalDuration = null;
+			if (archiveInterval != null) {
+				try {
+					archiveIntervalDuration = Duration.ofMillis(Long.parseLong(archiveInterval.getLabel()));
+				} catch (NumberFormatException nfe) {
+					try {
+						archiveIntervalDuration = Duration.parse(archiveInterval.getLabel());
+					} catch (DateTimeParseException dtpe) {
+						// ignore
+					}
+				}
+				if (archiveIntervalDuration == null) {
+					log.error("invalid archive interval: {}", archiveInterval);
+				}
+			}
+
+			return new KvinPartitioned(archivePath, archiveIntervalDuration);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	@Iri("plugin://io.github.linkedfactory.service/data/archiveInterval")
-	public abstract int getArchiveInterval();
+	public abstract ILiteral getArchiveInterval();
 }
