22588a1c43c7b154d4220f5419f5553c8937e568
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2024-08-09 12:00:39 +0200

    Simplify and fix JSON format parser.

--- bundles/io.github.linkedfactory.core/src/main/java/io/github/linkedfactory/core/kvin/util/JsonFormatParser.java
+++ bundles/io.github.linkedfactory.core/src/main/java/io/github/linkedfactory/core/kvin/util/JsonFormatParser.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.github.linkedfactory.core.kvin.Kvin;
 import io.github.linkedfactory.core.kvin.KvinTuple;
 import io.github.linkedfactory.core.kvin.Record;
@@ -21,17 +20,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.*;
+import java.util.Iterator;
+import java.util.Map;
 
 public class JsonFormatParser {
     final static Logger logger = LoggerFactory.getLogger(JsonFormatParser.class);
     final static JsonFactory jsonFactory = new JsonFactory().configure(Feature.AUTO_CLOSE_SOURCE, true);
     final static ObjectMapper mapper = new ObjectMapper()
-               .configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true);
-    JsonParser jsonParser;
+            .configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true);
+    JsonParser parser;
 
     public JsonFormatParser(InputStream content) throws IOException {
-        jsonParser = jsonFactory.createParser(content);
+        parser = jsonFactory.createParser(content);
     }
 
     public NiceIterator<KvinTuple> parse() {
@@ -43,42 +43,67 @@ public class JsonFormatParser {
             KvinTuple kvinTuple;
             URI currentItem;
             URI currentProperty;
-            boolean isLoopingProperties = false;
-            boolean isLoopingPropertyItems = false;
+            State state = State.PARSE_ITEMS;
 
             @Override
             public boolean hasNext() {
+                if (kvinTuple != null) {
+                    return true;
+                }
                 try {
-                    if (kvinTuple == null && isLoopingPropertyItems) {
-                        boolean isAddingPropertyItems = addNextPropertyItem(currentItem, currentProperty);
-                        if (!isAddingPropertyItems) {
-                            boolean isAddingProperty = addNextProperty();
-                            if (!isAddingProperty) {
-                                addNextItem();
-                            }
+                    JsonToken token = null;
+                    do {
+                        switch (state) {
+                            case PARSE_ITEMS:
+                                while ((token = parser.nextToken()) != null) {
+                                    if (token == JsonToken.FIELD_NAME) {
+                                        currentItem = URIs.createURI(parser.currentName());
+                                        state = State.PARSE_PROPERTIES;
+                                        break;
+                                    }
+                                }
+                                break;
+                            case PARSE_PROPERTIES:
+                                while ((token = parser.nextToken()) != null) {
+                                    if (token == JsonToken.FIELD_NAME) {
+                                        currentProperty = URIs.createURI(parser.currentName());
+                                        state = State.PARSE_VALUES;
+                                        break;
+                                    } else if (token == JsonToken.END_OBJECT) {
+                                        state = State.PARSE_ITEMS;
+                                        break;
+                                    }
+                                }
+                                break;
+                            case PARSE_VALUES:
+                                while ((token = parser.nextToken()) != JsonToken.END_ARRAY && token != null) {
+                                    if (token == JsonToken.START_OBJECT) {
+                                        JsonNode node = mapper.readTree(parser);
+                                        Object value = nodeToValue(node.get("value"));
+                                        Object seqNr = nodeToValue(node.get("seqNr"));
+                                        Number time = (Number) nodeToValue(node.get("time"));
+                                        if (value != null) {
+                                            kvinTuple = new KvinTuple(currentItem, currentProperty, Kvin.DEFAULT_CONTEXT,
+                                                    time != null ? time.longValue() : currentTime,
+                                                    seqNr != null ? ((Number) seqNr).intValue() : 0, value);
+                                            break;
+                                        } else {
+                                            throw new IOException(String.format("Invalid null value for item %s and property %s", currentItem, currentProperty));
+                                        }
+                                    }
+                                }
+                                if (token == JsonToken.END_ARRAY) {
+                                    state = State.PARSE_PROPERTIES;
+                                }
+                                break;
                         }
-                        return isTuplesAlreadyGenerated();
-
-                    } else if (kvinTuple == null && isLoopingProperties) {
-                        boolean isAddingProperty = addNextProperty();
-                        if (!isAddingProperty) {
-                            addNextItem();
-                        }
-                        return isTuplesAlreadyGenerated();
-
-                    } else if (kvinTuple == null) {
-                        addNextItem();
-                        return isTuplesAlreadyGenerated();
-
-                    } else {
-                        return isTuplesAlreadyGenerated();
-                    }
+                    } while (kvinTuple == null && token != null);
                 } catch (Exception e) {
                     logger.error("Exception while parsing", e);
                     try {
-                        if (jsonParser != null) {
-                            jsonParser.close();
-                            jsonParser = null;
+                        if (parser != null) {
+                            parser.close();
+                            parser = null;
                         }
                     } catch (IOException ioe) {
                         // ignore
@@ -86,6 +111,7 @@ public class JsonFormatParser {
                     }
                     throw new RuntimeException(e);
                 }
+                return kvinTuple != null;
             }
 
             @Override
@@ -95,71 +121,12 @@ public class JsonFormatParser {
                 return tuple;
             }
 
-            private void addNextItem() throws IOException {
-                while (jsonParser.nextToken() != null) {
-                    if (jsonParser.getCurrentToken() == JsonToken.FIELD_NAME) {
-                        currentItem = URIs.createURI(jsonParser.getCurrentName());
-                        addNextProperty();
-                        if (isTuplesAlreadyGenerated()) {
-                            break;
-                        }
-                    }
-                }
-            }
-
-            private boolean addNextProperty() throws IOException {
-                while (jsonParser.nextToken() != null) {
-                    isLoopingProperties = true;
-                    if (jsonParser.getCurrentToken() == JsonToken.FIELD_NAME) {
-                        currentProperty = URIs.createURI(jsonParser.getCurrentName());
-                        jsonParser.nextToken();
-
-                        addNextPropertyItem(currentItem, currentProperty);
-                        jsonParser.skipChildren();
-                    }
-                    if (isTuplesAlreadyGenerated()) break;
-                }
-                if (kvinTuple == null && jsonParser.getCurrentToken() == JsonToken.END_OBJECT) {
-                    isLoopingProperties = false;
-                }
-                return isTuplesAlreadyGenerated();
-            }
-
-            private boolean addNextPropertyItem(URI item, URI property) throws IOException {
-                JsonToken token;
-                while ((token = jsonParser.nextToken()) != JsonToken.END_ARRAY && token != null) {
-                    isLoopingPropertyItems = true;
-                    if (jsonParser.getCurrentToken() == JsonToken.START_OBJECT) {
-                        JsonNode node = mapper.readTree(jsonParser);
-                        Object value = nodeToValue(node.get("value"));
-                        Object seqNr = nodeToValue(node.get("seqNr"));
-                        Number time = (Number) nodeToValue(node.get("time"));
-                        if (value != null) {
-                            kvinTuple = new KvinTuple(item, property, Kvin.DEFAULT_CONTEXT,
-                                    time != null ? time.longValue() : currentTime,
-                                    seqNr != null ? ((Number) seqNr).intValue() : 0, value);
-                            break;
-                        } else {
-                            throw new IOException(String.format("Invalid null value for item %s and property %s", item, property));
-                        }
-                    }
-                }
-                if (kvinTuple == null && jsonParser.getCurrentToken() == JsonToken.END_ARRAY) {
-                    isLoopingPropertyItems = false;
-                }
-                return isTuplesAlreadyGenerated();
-            }
-
-            private boolean isTuplesAlreadyGenerated() {
-                return kvinTuple != null;
-            }
-
             @Override
             public void close() {
                 try {
-                    if (jsonParser != null) {
-                        jsonParser.close();
-                        jsonParser = null;
+                    if (parser != null) {
+                        parser.close();
+                        parser = null;
                     }
                 } catch (IOException e) {
                     // ignore
@@ -209,5 +176,9 @@ public class JsonFormatParser {
         } else {
             return node;
         }
+    }
+
+    enum State {
+        PARSE_ITEMS, PARSE_PROPERTIES, PARSE_VALUES
     }
 }
