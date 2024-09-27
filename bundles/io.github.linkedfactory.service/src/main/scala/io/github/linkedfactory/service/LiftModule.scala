8d7c178932aa9be0f836d8977dee5b86eaddc268
Marko Friedemann <marko.friedemann@iwu.fraunhofer.de>
2024-09-27 15:57:01 +0200

    auth: support a simple API token
    support simple API tokens to run with user access rights
    - just uses external id set for user accounts
    - does not offer a UI for adding/managing tokens
    - example jaas.ttl for predefined token:
      user:token-demo
        a foaf:Agent ; auth:password "${super-secret-password}" ;
        auth:externalId <enilink:jaas:token:${token}> ;
      .

--- bundles/io.github.linkedfactory.service/src/main/scala/io/github/linkedfactory/service/LiftModule.scala
+++ bundles/io.github.linkedfactory.service/src/main/scala/io/github/linkedfactory/service/LiftModule.scala
@@ -17,23 +17,29 @@ package io.github.linkedfactory.service
 
 import io.github.linkedfactory.core.kvin.Kvin
 import io.github.linkedfactory.core.rdf4j.ContextProvider
-import net.enilink.komma.core.{IReference, URI, URIs}
+import net.enilink.komma.core.{IEntityManager, IReference, URI, URIs}
 import net.enilink.komma.em.concepts.IResource
+import net.enilink.komma.model.IModelSet
 import net.enilink.platform.lift.LiftService
 import net.enilink.platform.lift.sitemap.{AddMenusAfter, Menus}
 import net.enilink.platform.lift.util.Globals
+import net.enilink.platform.security.auth.{AccountHelper, EnilinkPrincipal}
 import net.liftweb.common.{Box, Empty, Full}
 import net.liftweb.http.LiftRulesMocker.toLiftRules
 import net.liftweb.http._
 import net.liftweb.sitemap.Loc.{EarlyResponse, Hidden, Link}
 import net.liftweb.sitemap.LocPath.stringToLocPath
 import net.liftweb.sitemap.{Loc, Menu, SiteMap}
-import net.liftweb.util.Helpers
+import net.liftweb.util.{Helpers, DynoVar, LoanWrapper}
 import net.liftweb.util.Helpers.tryo
 import org.osgi.framework.{FrameworkUtil, ServiceReference}
 import org.osgi.util.tracker.ServiceTracker
 
-import java.util
+import java.util.{Arrays, Hashtable}
+import java.util.concurrent.atomic.AtomicReference
+import java.security.PrivilegedAction
+import javax.security.auth.Subject
+
 import scala.language.implicitConversions
 
 /**
@@ -63,6 +69,8 @@ class LiftModule {
   }
 
   var shutdownHooks: List[() => Any] = Nil
+  var msRef: ServiceReference[IModelSet] = null
+  var emRef: AtomicReference[IEntityManager] = new AtomicReference(null)
 
   def boot {
     // provide current context for Kvin operations
@@ -84,7 +92,7 @@ class LiftModule {
     // FIXME: initialize value store differently (not as side-effect of Data ctor)
     Data.kvin map { kvin =>
       shutdownHooks :+= (() => kvin.close())
-      val kvinSvc = FrameworkUtil.getBundle(getClass).getBundleContext.registerService(classOf[Kvin], kvin, new util.Hashtable[String, Object]())
+      val kvinSvc = FrameworkUtil.getBundle(getClass).getBundleContext.registerService(classOf[Kvin], kvin, new Hashtable[String, Object]())
       shutdownHooks :+= (() => kvinSvc.unregister())
       LiftRules.statelessDispatch.append(new KvinService("linkedfactory" :: Nil, kvin))
     }
@@ -127,6 +135,53 @@ class LiftModule {
         }.open()
       }
     }
+
+    // support a simple API token, run with access rights of its user
+    S.addAround(new LoanWrapper {
+      private object DepthCnt extends DynoVar[Boolean]
+
+      def apply[T](f: => T): T = if (DepthCnt.is == Full(true)) f
+      else DepthCnt.run(true) {
+        try {
+          S.request.flatMap(_.header("Authorization")).flatMap { token =>
+            val tokenId = URIs.createURI(s"enilink:jaas:token:$token")
+            Option(AccountHelper.findUser(getEntityManager, Arrays.asList(tokenId))).flatMap { user =>
+              val userId = user.getURI
+              val enilinkPrincipal = new EnilinkPrincipal(userId)
+              val subject = new Subject
+              subject.getPrincipals.add(enilinkPrincipal)
+              Full(Subject.doAs(subject, new PrivilegedAction[T] {
+                override def run = f
+              }))
+            }
+          } openOr f
+        } catch {
+          case _: Throwable => f
+        }
+      }
+    })
+    shutdownHooks :+= (() => {
+      if (emRef.get != null) {
+        FrameworkUtil.getBundle(getClass).getBundleContext.ungetService(msRef)
+        emRef.set(null)
+        msRef = null
+      }
+    })
+  }
+
+  // borrowed from EnilinkLoginModule
+  def getEntityManager(): IEntityManager = {
+    if (emRef.get == null) {
+      val ctx = FrameworkUtil.getBundle(getClass).getBundleContext
+      msRef = ctx.getServiceReference(classOf[IModelSet])
+      if (msRef != null) {
+        val modelSet = ctx.getService(msRef)
+        if (modelSet != null) {
+          emRef.set(modelSet.getMetaDataManager)
+        }
+      }
+    }
+    emRef.get
   }
 
   def shutdown {
